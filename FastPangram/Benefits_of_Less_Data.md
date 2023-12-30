a19438acf5d6a989773cca60b339908ea496f415
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2023-12-30 22:23:30 +0000

    implemented second approach to say whether a line has all 26 chars of lowercase english letter in 26 bits where 1 mean the letter exists, 0 means it doesn't

---
+++ FastPangram/Benefits_of_Less_Data.md
@@ -0,0 +1,10 @@
+Benefits of Storing Data in Smaller Sizes
+
+- Using less data means less space required
+- A lot of times, less space means less time
+- With smaller data, you might be able to store it in registers closer to the CPU
+
+Thinking about runtime
+- You can't just think about time complexity
+- You also have to think about latency of the round trips that the CPU/computation has to make to storage
+such as RAM, registers, etc. to fetch data as those affect the overall latency
--- FastPangram/fast-pangram/pangram.c
+++ FastPangram/fast-pangram/pangram.c
@@ -1,14 +1,83 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h> 
+#include <assert.h>
 
+int turnBitAtIndexOn(int number, int idx) {
+  //Example 1: Input: number = 0b00000000000000000000000000, idx = 0
+  //Output: number = 0b00000000000000000000000001
 
-bool ispangram(char *s) {
-  // TODO implement this!
-  return false;
+  //Example 2: number = 0b00000000000000000000000000, idx = 3
+  //Output: number = 0b00000000000000000000001000
+
+  //Algorithm:
+  //initialize int lightSwitch = 1
+  //lightSwitch << idx
+  //number &= 
+  int lightSwitch = 1;
+  lightSwitch = lightSwitch << idx;
+  number &= lightSwitch;
+  return number;
 }
 
+bool ispangram(char *s) {
+  // Algorithm:
+  //Check if there is atleast one occurrence of the each letter 
+  //in the set of lower case english alphabet letters
+  //in the string that *s points to
+
+  //Approach 1:
+  //initialize int[] counts of size 26
+  //Iterate memory address by memory address starting from the first char
+  //of the line that char pointer s points to 
+  //convert each char to lowercase
+  //get ascii value of lowercase char
+  //normalize ascii value to range 0-25
+  //if isalpha(), increment value at counts[idx]
+  //iterate through counts
+  //if counts[i] == 0
+  //return false
+  //at the end, return true
+
+
+  //Approach 2:
+  //Use 26 bits to represent whether there exists a letter a-z 
+  //in this line. 1 means the character at this "index" in the alphabet
+  //exists in this line, 0 means it doesn't
+  //for ex. "abcdef" could be represented by "00000000000000000000111111"
+  //iterate through each char in the line
+  //if char is in the alphabet
+  //convert to lowercase version
+  //turn the bit corresponding to this lowercase letter that is one of the letters from a-z on 
+  //at that index
+  int counts = 0b00000000000000000000000000;
+  int pangramCounts = 0b11111111111111111111111111;
+  char *t = s;
+  for (char c = *t; c != '\0'; c=*(++t)) {
+    if (isalpha(c)) {
+      char lowercaseLetter = tolower(c);
+      int idx = lowercaseLetter - 97;
+      turnBitAtIndexOn(counts, idx);
+    }
+  }
+
+  return (counts < pangramCounts) ? false: true;
+}
+
+
+
+
 int main() {
+
+  // char* test1 = "abcdef";
+  // assert(ispangram(test1) == false);
+  // printf("ok\n");
+
+  // char* test2 = "Vamp fox held quartz duck just by wing.";
+  // assert(ispangram(test2) == true);
+  // printf("ok");
+
   size_t len;
   ssize_t read;
   char *line = NULL;
